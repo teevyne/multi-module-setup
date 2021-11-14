ce15599d7d9309b0b3f0d4eb41dbfb10e6f0df3d
Tolulope Ayemobola <ayemobolatolulope@gmail.com>
2021-11-14 22:11:41 +0100

    completed for submission

---
+++ src/main/java/com/project/demohospital/DemohospitalApplication.java
@@ -0,0 +1,13 @@
+package com.project.demohospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemohospitalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemohospitalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/demohospital/DemohospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.demohospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemohospitalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
